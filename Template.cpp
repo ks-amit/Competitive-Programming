4d8223663ef2e8f0f91dd0d43176ff75162cee58
Amit K S <1998amit@gmail.com>
2017-10-09 21:29:04 +0530

    Add files via upload

--- Template.cpp
+++ Template.cpp
@@ -11,7 +11,64 @@ using namespace std;
 #define speed ios::sync_with_stdio(false)
 
 
+// FAST I/O
+
+void fastscan(int &x){
+	register int c = getchar_unlocked();
+	x = 0;
+	for(; (c<48 || c>57); c = getchar_unlocked());
+	for(; c>47 && c<58; c = getchar_unlocked()){
+		x = (x<<1) + (x<<3) + c - 48;
+	}
+}
+
+// GCD - EUCLID'S ALGORITHM
+
+ll gcd(ll a, ll b){
+	if(b == 0){
+		return a;
+	}
+	else{
+		return gcd(b, a % b);
+	}
+}
+
+// MILLER RABIN PRIMALITY TEST
+
+bool millerrabin(ll n){
+	if(n <=1 || n % 2 == 0){
+		if(n != 2){
+			return false;
+		}
+	}
+	if(n == 2 || n == 3){
+		return true;
+	}
+	ll d = n-1;
+	while(d % 2 == 0){
+		d /= 2;
+	}
+	ll a[12] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+	for(int i = 0; i < 12 && a[i] < n; ++i){
+		ll temp = d;
+		ll mod = largepow(a[i], temp, n);
+		if( mod == 1 || mod == n-1){
+			continue;
+		}
+		while(temp != n-1 && mod != n-1){
+			mod = fastmul(mod, mod, n);
+			temp *= 2;
+		}
+		if(mod != n-1){
+			return false;
+		}
+	}
+	return true;
+}
+
 // DISJOINT SUBSET
+
+// two arrays arr[] and size[] of size <n> required
 
 void setupDS(int arr[], int size[], int n){
 	for(int i = 0; i < n; i++){
