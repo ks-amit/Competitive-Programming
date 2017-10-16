52da50a25075a0cbac3aba076cf8fe4208ef1b81
Amit K S <1998amit@gmail.com>
2017-10-16 21:34:04 +0530

    COPY PASTE STUFF :p

--- Template.cpp
+++ Template.cpp
@@ -10,8 +10,10 @@ using namespace std;
 #define MOD 1000000007
 #define speed ios::sync_with_stdio(false)
 
+const int N = 100005;
 
-// FAST I/O - INTEGER SCAN
+
+// FAST I/O
 
 void fastscan(int &x){
 	register int c = getchar_unlocked();
@@ -30,6 +32,87 @@ ll gcd(ll a, ll b){
 	}
 	else{
 		return gcd(b, a % b);
+	}
+}
+
+// EXTENDED EUCLID'S ALGORITHM - aX + bY = gcd(a, b)
+
+ll d, x, y;
+
+ll egcd(ll a, ll b){
+	if(b == 0){
+		d = a;
+		x = 1;
+		y = 0;
+		return a;
+	}
+	else{
+		d = egcd(b, a % b);
+		ll temp = x;
+		x = y;
+		y = temp - ((a / b) * y);
+		return d;
+	}
+}
+
+// POWER FUNCTION ( BINARY EXPONENTIATION ) - CALCULATES (a ^ b) % c
+
+ll bin(ll a, ll b, ll c){
+	ll ans = 1;
+	while(b > 0){
+		if(b % 2 == 1){
+			ans = ((ans % c) * (a % c)) % c;
+		}
+		a = ((a % c) * (a % c)) % c;
+		b /= 2;
+	}
+	return ans;
+}
+
+// OVERFLOW MULTIPLICATION - CALCULATES (a * b) % c
+
+ll mul(ll a, ll b, ll c){
+	ll res = 0;
+	while(b > 0){
+		if(b % 2 == 1){
+			res = ((res % c) + (a % c)) % c;
+		}
+		a = ((a % c) + (a % c)) % c;
+		b /= 2;
+	}
+	return res;
+}
+
+// POWER FUNCTION - OVERFLOW CASE - CALCULATES (a ^ b) % c
+
+ll bin1(ll a, ll b, ll c){
+	ll ans = 1;
+	while(b > 0){
+		if(b % 2 == 1){
+			ans = mul(ans, a, c) % c;
+		}
+		a = mul(a, a, c) % c;
+		b /= 2;
+	}
+	return ans;
+}
+
+// MODULAR MULTIPLICATIVE INVERSE ( where c = prime )
+
+ll inv(ll b, ll c){
+	return bin(b, c - 2, c) % c;
+} 
+
+// MODULAR MULTIPLICATIVE INVERSE ( where gcd(b, c) = 1 )
+
+ll inv1(ll b, ll c){
+	if(egcd(b, c) != 1){
+		return -1;
+	}
+	else{
+		egcd(b, c);
+		ll res = ((x % c) + c) % c;
+		return res;
 	}
 }
 
@@ -95,5 +178,53 @@ void unionDS(int arr[], int size[], int n, int A, int B){
 	else{
 		arr[rootB] = arr[rootA];
 		size[rootA] += size[rootB];
+	}
+}
+
+// KMP STRING MATCHING ALGORITHM
+
+int failure[N];
+
+void buildFA(string pattern){
+	int i = 1, j = 0;
+	failure[0] = 0;
+	while(i < pattern.size()){
+		if(pattern[i] == pattern[j]){
+			failure[i] = j + 1;
+			i++; j++;
+		}
+		else{
+			if(j != 0){
+				j = failure[j - 1];
+			}
+			else{
+				failure[i] = 0;
+				i++;
+			}
+		}
+	}
+}
+
+void kmp(string text, string pattern){ // search for pattern in text
+	int m = text.size(); int n = pattern.size();
+	buildFA(pattern);
+	int i = 0, j = 0;
+	while(i < m){
+		if(pattern[j] == text[i]){
+			i++; j++;
+		}
+		if(j == n){
+			printf("FOUND AT %d\n", i - j);
+			j = failure[j - 1];
+		}
+
+		else if(i < m && pattern[j] != text[i]){
+			if(j != 0){
+				j = failure[j - 1];
+			}
+			else{
+				i++;
+			}
+		}
 	}
 }
